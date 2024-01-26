28ce8e922fa7ba4a4b6df4970a5ab9286a5c60fa
Entropy <entropy1110@gmail.com>
2024-01-27 03:02:54 +0900

    Added Ulog Listener & Consumer

--- UlogParser.py
+++ UlogParser.py
@@ -1,12 +1,19 @@
 import re
 import queue
 from datetime import datetime
+import time
+import socket
+from threading import Event, Lock
 
 
 class UlogParser:
     def __init__(self):
         self.parsed_logs_queue = queue.Queue()  # a queue for a single parsed logs
         self.ulog_str = ""
+        self.ulog_socket = 0
+        self.lock = Lock()
+        self.loop = 1
+        self.event = Event()
         self.replace_to_current_time = 1  # If 1: replace the timestamp of parsed logs to current timestamp. Else : do nothing.
 
     # Split(\n) and parse each log strings, and put them in parsed_logs_queue.
@@ -21,7 +28,7 @@ class UlogParser:
 
     def parse_log_line(self, line):
         # Regular expression
-        pattern = r"(?:(\x1b\[\d\;\d{2}m))?(?P<time>\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3})\s+(?P<code>[IEWC])\s+(?P<binary>.*?)\s*:\s*(?P<desc>.*)"
+        pattern = r"(?:(\x1b\[\d{1,}\;\d{2}m))?(?P<time>\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3})\s+(?P<code>[IEWC])\s+(?P<binary>.*?)\s*:\s*(?P<desc>.*)"
 
         # if not isinstance(line, bytes):
         #     line = line.encode("utf-8")
@@ -50,3 +57,66 @@ class UlogParser:
             return None
         else:
             return self.parsed_logs_queue.get()
+
+    def connect_tcp(self, ip, port):
+        self.ulog_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.ulog_socket.connect((ip, port))
+        return self.ulog_socket
+
+    def recvline(self):
+        try:
+            if self.ulog_socket:
+                pass
+            else:
+                print("ULOG_SOCKET NOT INITIALIZED")  # debug
+                return
+            self.lock.acquire()
+            # print("recvline lock acquired")  # debug
+            line = b""
+            while True:
+                data = self.ulog_socket.recv(1)
+                line += data
+                if data == b"\n":
+                    self.lock.release()
+                    # print(line.decode() + "  recvline lock released")  # debug
+                    return line
+        except TypeError:
+            return b""
+
+    def listen(self, timeout, recved_lines: list):
+        lines = b""
+        if timeout > 0:
+            self.pause_listening_loop()
+            end_time = time.time() + timeout
+            while time.time() < end_time:
+                # 데이터 수신 후 반환
+                line = self.recvline()
+                if line != b"":
+                    lines += line
+                # print(lines.decode())
+            recved_lines[0] = lines
+            self.resume_listening_loop()
+            return recved_lines
+        elif timeout == 0:
+            while not self.event.is_set():
+                if self.loop == 1:
+                    pass
+                else:
+                    continue
+                recved_lines.clear()
+                line = self.recvline()
+                # if line != b"":
+                #     #print(line.decode())
+                #     recved_lines.append(line)
+            self.event.clear()
+        else:
+            return
+
+    def return_listening_loop(self):
+        self.event.set()
+
+    def pause_listening_loop(self):
+        self.loop = 0
+
+    def resume_listening_loop(self):
+        self.loop = 1
